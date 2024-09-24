8db2811688ec89f28b64d9d5803979a8a333b38d
sauveerk <sauveer.ketan@gmail.com>
2024-09-24 23:02:26 +0530

    added comments to backend file

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-backend-xdf3489xz "
+    bucket         = "terraform-state-backend-xdf3489xz"
     key            = "ec2-provision/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
