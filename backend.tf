7485398925f3cbf0f3708fa8db79e36809e194f1
sauveerk <sauveer.ketan@gmail.com>
2024-09-24 23:51:24 +0530

    removed comment from workflow file

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-backend-xdf3489xz"
-    key            = "ec2-provision/terraform.tfstate-${github.run_id}"
+    key            = "ec2-provision/terraform.tfstate-${env.TF_STATE_FILE}"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "tfstate-locking"
